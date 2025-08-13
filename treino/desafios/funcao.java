d9b4286588548743d00542a1dcbadfad343d986c
Sammuel Victor <samvss13@gmail.com>
2025-08-13 14:42:14 +0000

    1142

--- treino/desafios/funcao.java
+++ treino/desafios/funcao.java
@@ -15,20 +15,33 @@ public class funcao {
     int b = ler.nextInt();
     System.out.println("digite o numero 3:");
     int c = ler.nextInt();
-    System.out.println(raiz(a, b, c));
+    ler.close();
+    calcraiz(a, b, c);
     }
  
-
-    public static double raiz (int a, int b, int c){
-    double delta = b*b - 4 * a * c;
-    double raiz = (-b + Math.sqrt(delta))/ 2 * a;
-    System.out.println(raiz);
-    return raiz;
+    public static double calcdelta (int a, int b, int c){
+    return (b*b) - 4.0 * a * c;
     }
 
+
+    public static void calcraiz (int a, int b, int c){
+    double delta = calcdelta(a, b, c);
+    System.out.println("\nO valor de delta é: " + delta);
+    if (delta < 0) {
+      System.out.println("\nNão há raízes reais.");
+    } else if (delta == 0) {
+      System.out.println("\nHá apenas uma raiz real.");
+      double x = -b / (2 * a);
+      System.out.println("\nA raiz é: " + x);
+    } else {
+      double raiz1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+      double raiz2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+      System.out.println("\nas raízes são: " + raiz1 + " e " + raiz2);
+    }
+
+    }
 }    
 
 
 
-  //(x = (-b ± √Δ) / 2a) 
 
