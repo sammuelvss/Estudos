4938f4dce9c097d9f2b7d127d919a5377e48cabf
Sammuel Victor <samvss13@gmail.com>
2025-08-19 20:07:34 +0000

    1707

--- lista0.java
+++ lista0.java
@@ -1,6 +1,4 @@
-
 import java.util.Scanner;
-
 
 public class lista0 {
     public static void main(String[] args) {
@@ -28,7 +26,7 @@ public class lista0 {
         // QUESTÃO 02
 
 
-        Scanner ler = new Scanner(System.in);
+        /*Scanner ler = new Scanner(System.in);
         int cont = 0;
         System.out.println("digite o primeiro número:");
         int num1 = ler.nextInt();
@@ -48,11 +46,62 @@ public class lista0 {
             System.out.println(num1 + "," + num3 + "," + num2);
         } else if (num2 >= num1 && num1 <= num3) {
             System.out.println(num2 + "," + num3 + "," + num1);
-        }
+        }*/
+
+
+        //QUESTÂO 3
+
+
+    /*Scanner ler = new Scanner(System.in);
+    System.out.println("calcular raiz:");
+    System.out.println("digite o numero 1:");
+    int a = ler.nextInt();
+    System.out.println("digite o numero 2:");
+    int b = ler.nextInt();
+    System.out.println("digite o numero 3:");
+    int c = ler.nextInt();
+    ler.close();
+    calcraiz(a, b, c);
+    }
+
+    public static double calcdelta (int a, int b, int c){
+    return (b*b) - 4.0 * a * c;
+    }
+
+
+    public static void calcraiz (int a, int b, int c){
+    double delta = calcdelta(a, b, c);
+    System.out.println("\nO valor de delta é: " + delta);
+    if (delta < 0) {
+    System.out.println("\nNão há raízes reais.");
+    } else if (delta == 0) {
+    System.out.println("\nHá apenas uma raiz real.");
+      double x = -b / (2 * a);
+    System.out.println("\nA raiz é: " + x);
+    } else {
+      double raiz1 = (-b + Math.sqrt(delta)) / (2.0 * a);
+      double raiz2 = (-b - Math.sqrt(delta)) / (2.0 * a);
+    System.out.println("\nas raízes são: " + raiz1 + " e " + raiz2);
+    }*/
 
 
 
+    //QUESTÃO 4
 
+
+    Scanner ler = new Scanner(System.in);
+    System.out.println("digite o dia:");
+    int dia = ler.nextInt();
+    System.out.println("digite o mês:");
+    int mes = ler.nextInt();
+    System.out.println("digite o ano:");
+    int ano = ler.nextInt();
+    
+    if (dia >= 21 && dia <= 19 && mes == 3 || mes == 4) {
+        System.out.println("ARIES");
+    } else if (dia >= 20 && dia <= 20 && mes == 4 || mes == 5 ){
+        System.out.println("TOURO");
+    }
 
 
 
