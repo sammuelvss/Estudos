3674c446466d1a31e0b7e4d00183f937b4b80bbd
Sammuel Victor <samvss13@gmail.com>
2025-08-20 19:18:29 +0000

    1618

--- chicoNoClima/site.js
+++ chicoNoClima/site.js
@@ -13,21 +13,36 @@ carro.cor="vermelho";
 carro.ano= 2025;
 delete carro.cor;
 
-  function ligar()  {
-
+  carro.ligar = function() {
   console.log('o carro está ligado!');
-  
-}
-
-carro.ligar = ligar();
-
+};
+carro.ligar();
 
 console.log(carro);
 
+for (let propiedades in carro) {
+  console.log(propiedades + ": " + carro[propiedades]);
+}
 
+let pessoa = {
+  nome: "sammuel",
+  endereco: {
+  rua: "rua popular",
+  cidade: "Recife"
+  }
+};
+console.log(pessoa);
 
+console.log(Object.keys(carro));
+console.log(Object.values(carro));
 
+function Animal(nome, tipo) {
+  this.nome = nome;
+  this.tipo = tipo;
+}
+let Galo = new Animal("Garnize", "Galo");
 
+let vaca = new Animal("Mimosa", "Vaca");
 
-
-
+console.log(Galo);
+console.log(vaca);
