b7e2a74733fb8c0305b2c919a10fa05e4586806d
Sammuel Victor <samvss13@gmail.com>
2025-08-23 02:49:20 +0000

    2349

--- q3.java
+++ q3.java
@@ -2,16 +2,14 @@
 
 
 public class q3 {
-
-  
     public static void main(String[] args) {
 
-int [] a = int {1,2,3,4,5};
-int [] b = int {2,4,6,7,8};
+int [] a =  {1,2,3,4,5};
+int [] b =  {2,4,6,7,8};
 int [] c = new int[5];
 
 for (int i = 0; i < 5; i++) {
-    c[i]=a[i]+b[i];
+    c[i] = a[i] + b[i];
  }
  for(int i=0; i<5; i++){
     System.out.println(c[i]);
