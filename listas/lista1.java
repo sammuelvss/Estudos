1a58154c08b26f23479df2dbd2f08e7216b8f03c
Sammuel Victor <samvss13@gmail.com>
2025-08-29 20:20:25 +0000

    1720

--- listas/lista1.java
+++ listas/lista1.java
@@ -1,16 +1,58 @@
 package listas;
 
+
+import java.util.Scanner;
 public class lista1 {
     
 public static void main(String[] args) {
     
 
+    //questão 01
+
+/*  int a[] = {1,0,5,-2,-5,7};
+    int soma = a[0] + a[1] + a[5];
+    System.out.println("a soma do valores do vetor é " + soma);
+    a[4] = 100;
+    for (int i = 0; i < 6; i++){
+    System.out.println("posição " + i + " do vetor é: " + a[i]);
+    }*/
 
 
+    //questão 02
 
+    
+    /*Scanner ler = new Scanner(System.in);
+    int [] v = new int [6];
+    for (int i = 0; i < v.length; i++) {
+        v[i] = ler.nextInt();
+    }
+    for (int i = 0; i < v.length; i++){
+        System.out.print(v[i] + ",");
+    }*/
+    
+
+    //questão 03
+
+    /*Scanner ler = new Scanner(System.in);
+    int [] numeros = new int [10];
+    for (int i = 0; i < numeros.length; i++) {
+        numeros[i] = ler.nextInt();   
+     }
+     for (int i =0; i < numeros.length; i++){  
+         if (numeros[i] % 2 == 0){
+                System.out.print(numeros[i] + ",");
+         }
+    }
+    System.out.println();*/
 
 
 
     
+    //questão 04
+
+
+
+
+
     }
 }
