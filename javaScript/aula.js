7d5b82b500892b7dc688b36d2d16211de83e5288
Sammuel Victor <samvss13@gmail.com>
2025-08-29 23:04:04 +0000

    2004

--- javaScript/aula.js
+++ javaScript/aula.js
@@ -38,12 +38,55 @@ console.log("vamos ficar em casa:",ficarEmCasa);*/
 
 
 
-var texto = window.confirm("escolha uma");
+/*var texto = window.confirm("escolha uma");
 window.alert("você selecionou: " + texto);
 
 
 carros = new Array();
 carros[0] = "volvo";
 carros[5] = "BMW";
-carros[7] = "volkswagen";
+carros[7] = "volkswagen";*/
+
+
+const agenda = [null,null,null];
+
+let nome = prompt("digite o nome:");
+let telefone = prompt("digite o telefone:");
+let matricula = prompt("digite sua matricula");
+
+agenda[0] = nome;
+agenda[1] = telefone;
+agenda[2] = matricula;
+
+let remover = prompt("você deseja remover? digite 1 para nome, 2 para telefone, 3 para matricula.");
+if (remover == 1){
+    agenda.splice(0,1);
+    console.log(agenda);
+} else if (remover == 2){
+    agenda.splice(1,1);
+    console.log(agenda);
+} else if (remover == 3){
+    agenda.splice(2,1);
+    console.log(agenda);
+} else {
+    alert("numero invalido");
+}
+let adicionar = prompt("você deseja adicionar um dado? digite 1 para nome, 2 para telefone, 3 para matricula.");
+if (adicionar == 1){
+     nome2 = prompt("digite o nome:")
+    agenda.splice(0,0, nome2);
+} else if (adicionar == 2){
+    telefone2 = prompt("digite seu telefone:");
+    agenda.splice(1,0, telefone2);
+} else if (adicionar == 3){
+    matricula2 = prompt("digite sua matricula:");
+    agenda.splice(2,0, matricula2);
+} else {
+    alert("numero invalido.");
+}
+
+
+console.log(agenda);
+
+
 
