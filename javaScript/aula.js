3ab19889c3dfe95842b943e1886fffe5b0acea1e
Sammuel Victor <samvss13@gmail.com>
2025-09-06 00:21:40 +0000

    2122

--- javaScript/aula.js
+++ javaScript/aula.js
@@ -93,3 +93,6 @@ console.log(agenda);*/
 
 
 
+
+
+
--- questoes/aula.java
+++ questoes/aula.java
@@ -1,27 +1,63 @@
 package questoes;
 
 import java.util.Scanner;
-public class q3 {
-    public static void main(String[] args) {
+public class aula {
+    public static boolean verificaLinha(char[] linha, char c) {
 
-
-int v[][] = new int [3][3];
-for (int i = 0; i < 9; i++){
-
-}
-for (int i = 0; i < 3; i++ ){
-
-} for(int j = 0; j < 3; j++){
-    if(i==j){
-        System.out.println();
-
+        if(linha[0] == linha[1] && linha[1] == linha[2]){
+            if(linha[0] == c){
+            return true;
+            }
+        }
+        return false;
     }
-}
 
 
 
-    }  
-}
+    public static void main(String[] args) {
+        char[][] m = {
+
+            {'X','O','O'},
+            {'X','O','X'},
+            {'X','X','O'},
+
+        }
+        public static boolean verificaColuna(char[][] m, char c){
+        char temp[] = new char[3];
+        for(int i = 0; i < 3; i++){
+                for(int j = 0; j < 3; j++){
+                temp[j] = m[j][i];
+                }
+            }
+            verificaLinha(temp, c);
+            
+        
+    
+
+        //verificar linhas
+        int vencedor = -1;
+        for(int i = 0; i < m.length; i++){
+        if (verificaLinha(m[i], 'X')){
+            vencedor = 1;
+        } else if(verificaLinha(m[i], 'O')){
+            vencedor = 0;
+        }
+    }
+        //verificar coluna
+        verificaLinha(temp, 'O');
+        verificaColuna(temp, 'X');
+    
+
+
+        }
+    }
+
+
+
+
+
+    
+
 
 
 
