103db5dce663e9b125a9225f2504a43bed953c69
Sammuel Victor <samvss13@gmail.com>
2025-09-09 23:11:19 +0000

    2011

--- javaScript/aula.js
+++ javaScript/aula.js
@@ -47,8 +47,10 @@ carros[0] = "volvo";
 carros[5] = "BMW";
 carros[7] = "volkswagen";*/
 
-
-/*const agenda = [null,null,null];
+function adicionar(pessoa){
+switch (pessoa){
+case 1:
+const agenda  = [null,null,null];
 
 let nome = prompt("digite o nome:");
 let telefone = prompt("digite o telefone:");
@@ -57,6 +59,7 @@ let matricula = prompt("digite sua matricula");
 agenda[0] = nome;
 agenda[1] = telefone;
 agenda[2] = matricula;
+
 
 let remover = prompt("você deseja remover? digite 1 para nome, 2 para telefone, 3 para matricula.");
 if (remover == 1){
@@ -71,6 +74,7 @@ if (remover == 1){
 } else {
     alert("numero invalido");
 }
+
 let adicionar = prompt("você deseja adicionar um dado? digite 1 para nome, 2 para telefone, 3 para matricula.");
 if (adicionar == 1){
      nome2 = prompt("digite o nome:")
@@ -84,9 +88,28 @@ if (adicionar == 1){
 } else {
     alert("numero invalido.");
 }
+console.log(agenda);
+
+    }
+}
 
 
-console.log(agenda);*/
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
 
 
 
