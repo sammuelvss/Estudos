d7daf0bf6255764ecbd4c9638e8d4e968cf8428f
Sammuel Victor <samvss13@gmail.com>
2025-09-13 00:36:08 +0000

    2136

--- questoes/aula.java
+++ questoes/aula.java
@@ -3,7 +3,7 @@ package questoes;
 import java.util.Scanner;
 
         public class aula {
-    public static boolean verificaLinha(char[] linha, char c) {
+    /*public static boolean verificaLinha(char[] linha, char c) {
         return (linha[0] == c && linha[1] == c && linha[2] == c);
     }
 
@@ -60,12 +60,51 @@ import java.util.Scanner;
         else if (vencedor == 0) System.out.println("O venceu!");
         else System.out.println("Empate!");
     }
+}*/
+public static int fatorial(int n){
+    if(n == 1){
+        return 1;
+    }
+    else {
+       return n + fatorial(n - 1); 
+    }
+}
+
+public static int impares(int n){
+    if(n == 1){
+        return 1;
+    } else {
+        return (2 * n)-1 + impares(n - 1);
+    }
+}
+
+public static int potencia(int x, int n){
+    if(n == 0){
+        return 1;
+    }else {
+        return x * potencia(x, n - 1);
+    }
+
+}
+
+public static int fib(int n){
+    if(n == 1 || n == 2){
+        return 1;
+    }
+    else{
+        return fib(n - 2) + fib(n - 1);
+    }
+}
+
+
+    public static void main(String[] args) {
+        System.out.println(fib(10));
+    }
 }
 
 
 
-
-    
+        
 
 
 
