35df91357b780844c0f2c74e4b4ab45f9c7e7b59
Sammuel Victor <samvss13@gmail.com>
2025-10-07 03:31:30 +0000

    0031

--- listas/lista3.java
+++ listas/lista3.java
@@ -34,7 +34,7 @@ class lista3 {
     }*/
 
     //q2
-        Scanner ler = new Scanner(System.in);
+        /*Scanner ler = new Scanner(System.in);
         System.out.println("Digite a quantidade de livros na biblioteca:");
         int n = ler.nextInt();
         ler.nextLine(); 
@@ -47,7 +47,7 @@ class lista3 {
         String palavraChave = ler.nextLine();
         buscarLivros(livros, palavraChave, n);
         ler.close();
-        
+
        }   
        public static void buscarLivros(String[] livros, String palavraChave, int n){
         String nomeLivro = "";
@@ -60,8 +60,74 @@ class lista3 {
                 encontrado = true;
             } else if (!encontrado && i == n - 1) {
                 System.out.println("Nenhum livro encontrado");
-        }
+            }
        }
     }
-}
+}*/
+
+    //q3
+ 
+    Scanner ler = new Scanner(System.in);
+        System.out.println("Digite o tamanho do vetor:");
+        int n = ler.nextInt();
+        System.out.println("qual numero você quer buscar no vetor?");
+        int numero = ler.nextInt();
+
+        int [] v = new int[n];
+        for(int i = 0; i < n; i++){
+            v[i] = ler.nextInt();
+        }
+        ordenarVetor(v, n);
+        System.out.println("Vetor ordenado:");
+        for(int i = 0; i < n; i++){
+           System.out.println(v[i]);
+        }
+        System.out.println();
+        System.out.println("resultado:");
+        int [] busca = buscaBinaria(v, numero, n);
+        int indice = busca[0];
+        int passos = busca[1];
+        if(indice != -1){
+            System.out.println(indice + "," + passos);
+        } else {
+            System.out.println(indice);
+        }
+        
+    }
+        public static void ordenarVetor(int [] v, int n){
+
+            int temp = 0;
+            for(int i = 0; i < n; i++){
+                for(int j = 1; j < (n - i); j++){
+                    if(v[j - 1] > v[j]){
+                        temp = v[j -1];
+                        v[j - 1] = v[j];
+                        v[j] = temp;
+                    }
+                }
+            }
+        }
+
+        public static int[] buscaBinaria(int []v, int numero, int n){
+
+            int cont = 0;
+            int inicio = 0;
+            int fim = n - 1;
+            
+            while(inicio <=  fim){
+                int meio = (inicio + (fim -  inicio))/2;
+                
+                if(v[meio] == numero){
+                       return new int[] {meio, cont};
+                }
+                if (v[meio] < numero){
+                    inicio = meio + 1;
+                } else {
+                    fim =  meio -  1;
+                }
+                cont ++;
+            }
+            return new int[] {-1, cont};
+        }
+    }
 
