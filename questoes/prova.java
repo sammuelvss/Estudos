f96aa009e579848105330a78f6fc9b57a6229ee3
Sammuel Victor <samvss13@gmail.com>
2025-11-11 02:53:30 +0000

    2353

--- questoes/prova.java
+++ questoes/prova.java
@@ -165,7 +165,7 @@ public class prova {
                 return "não encontrado!";
             }
         }*/
-        Scanner ler = new Scanner(System.in);
+        /*Scanner ler = new Scanner(System.in);
         int n = ler.nextInt();
         int [] v = new int[n];
          for(int i = 0; i < v.length; i++){
@@ -180,7 +180,7 @@ public class prova {
             int n = v.length;
             for(int i = 0; i < n - 1; i++){
                 boolean parada = false; 
-                for(int j = 1; j < n; j++){
+                for(int j = 1; j < n - i; j++){
                     if(v[j-1] > v[j]){
                         int temp = v[j-1];
                         v[j-1] = v[j];
@@ -194,11 +194,153 @@ public class prova {
             }
             return n-1;
         }
+    }*/
+
+        /*int [] v  = {8,3,9,1,5};
+        selectionSortFocado(v);
     }
+        public static void selectionSortFocado(int[] v){
 
-
-
+            int n = v.length;
+            int menor = 0;
+            int temp;
+            for(int i = 0; i < n; i++){
+                menor = i;
+                boolean troca = false;
+                for(int j = i+1; j < n; j++){
+                    if(v[j] < v[menor]){
+                        menor =  j;
+                        troca = true;
+                    }
+                }
+                if(troca == true){
+                    temp = v[menor];
+                    v[menor] = v[i];
+                    v[i] = temp;
+                    System.out.println("Passagem i = " + i + " v[" + i + "] era " + v[menor] + ", depois da troca agora é: " + v[i]);
+                } else {
+                    System.out.println("Passagem i = " + i + ": Nenhuma troca, v[" + i + "] ja era o menor");
+                }
+            }
+        }
+    }*/    
     
+    
+    /*int [] v = {2,5,7,9,0};
+    int n = 4;
+    int x = 6;
+    insereOrdenado(v, n, x);
+    }
+    public static void insereOrdenado(int [] v, int n, int x){
+        
+    
+            int chave = x;
+            int j = n - 1;
+            while(j >= 0 && v[j] > chave){
+                v[j+1] = v[j];
+                j--;
+            }
+            v[j+1] = chave;
+        }
+    }*/
+    
+        /*public static int buscaBinaria(int[] v, int x){
 
+            int ini = 0;
+            int fim = v.length - 1;
+            
+            while(ini <= fim){
+                int meio = ((ini + fim)/2);
+                if(v[meio] == x){
+                    return meio;
+                } else if (v[meio] > x){
+                    fim = meio - 1;
+                } else {
+                    ini = meio + 1;
+                }
 
+            }
+            return -(ini)-1;
+        }
+    }*/
 
+    /*public static void bubbleSortReverso(int []v){
+
+        int temp;
+        int n = v.length;
+
+        for(int i = 0; i < n-1; i++){
+            for(int j = n-1; j > i; j--){
+                if(v[j] < v[j-1]){
+                    temp = v[j];
+                    v[j] = v[j-1];
+                    v[j-1] = temp;
+                }
+            }
+        }
+    }*/
+
+    /*int [] v = {5,4,3,2};
+    inserctionSort(v);
+    }
+    public static void inserctionSort(int [] v){
+
+        int cont = 0;
+        int total = 0;
+        int n = v.length;
+        for(int i = 1; i < n; i++){
+            int chave = v[i];
+            int j = i - 1;
+            cont = 0;
+            while(j >= 0  && v[j] > chave){
+                v[j + 1] = v[j];      
+                cont++;
+                j--; 
+            }
+            System.out.println("Passagem " + i + "(chave = " + v[i] + "): " + cont + "shifts.");
+            v[j+1] = chave;
+            total += cont;
+        }
+        System.out.println("Total de shifts:" + total);
+
+        }
+    }*/
+
+        /*public static void selectionSortDoFim(int [] v){
+            int n = v.length;
+            int temp;
+            for(int i = 0; i < n-1; i++){
+                int maior = 0;
+                for(int j = 1; j <= n - 1 - i; j++){
+                    if(v[j] > v[maior]){
+                        maior = j;
+                    }
+                }
+                temp = v[maior];
+                v[maior] = v[n-1-i];
+                v[n-1-i] = temp;
+            }
+        }*/
+
+        public static int buscaBinariaPrimeiraOcorrencia(int [] v, int x){
+            int cont = 0;
+            int ini = 0;
+            int fim = v.length-1;
+            int indice = 0;
+            while(ini <= fim){
+                int meio = ((ini + fim)/2);
+                if(v[meio] == x){
+                    indice += meio;
+                    cont++;
+                    if(cont >= 2){
+                        indice -= meio;
+                        return indice;
+                    }
+                } else if (v[meio] > x){
+                    fim = meio - 1;
+                } else {
+                    ini = meio + 1;
+                }
+            }
+            return -1;
+        }
